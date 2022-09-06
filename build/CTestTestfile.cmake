c77bd9847bc358d680791c21bd90bcba0ec963e3
chengyangkj <chengyangkj@qq.com>
2022-09-06 05:05:23 -0700

    rm build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chengyangkj/ros_apps/src/ros2_qt_demo
-# Build directory: /home/chengyangkj/ros_apps/src/ros2_qt_demo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
